0409344ff4c27f70e971b04a982b48e3912dded3
JBardoe <jackbardoe@gmail.com>
2026-02-20 20:09:46 +0000

    Reorganised headers

---
+++ include/parser/Parser.hpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <memory>
+#include <string>
+
+#include "parser/ELFFile.hpp"
+
+namespace ELFParser {
+
+/**
+ * Opens and parses an ELF file
+ *
+ * @param filepath relative or absolute path to the file
+ * @return pointer to the ELF file object
+ */
+std::unique_ptr<ELFFile> parseFile(const std::string& filepath);
+
+/**
+ * Parses the header of an ELF file
+ *
+ * @param filestream open stream to the file
+ * @return pointer to the ELF header object
+ */
+std::unique_ptr<ELFHeader> parseHeader(std::ifstream& filestream);
+}  // namespace ELFParser
