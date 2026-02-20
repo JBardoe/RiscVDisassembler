6100670b56a53290c5570957f7c7be17f58c5335
JBardoe <jackbardoe@gmail.com>
2026-02-20 20:40:59 +0000

    Added SymbolTable class

---
+++ include/disassembler/SymbolTable.hpp
@@ -0,0 +1,35 @@
+#ifndef SYMBOLTABLE_H
+#define SYMBOLTABLE_H
+
+#include <functional>
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "disassembler/RiscvTypes.hpp"
+
+namespace Disassembler {
+
+class SymbolTable {
+   public:
+    SymbolTable() = default;
+
+    void addSymbol(std::string name, uint32_t addr, uint32_t size,
+                   SymbolType type, SymbolBinding binding,
+                   std::string sectionName);
+    std::optional<std::reference_wrapper<const Symbol>> getSymbolName(
+        std::string name);
+    std::vector<Symbol> getSymbolAddr(uint32_t addr);
+    std::vector<Symbol> getSymbolSection(std::string sectionName);
+
+   private:
+    std::vector<Symbol> table;
+    std::unordered_map<std::string, int> nameLookup;
+    std::unordered_map<uint32_t, std::vector<int>> addrLookup;
+    std::unordered_map<std::string, std::vector<int>> sectionLookup;
+};
+
+}  // namespace Disassembler
+
+#endif
---
+++ src/disassembler/SymbolTable.cpp
@@ -0,0 +1,47 @@
+#include "disassembler/SymbolTable.hpp"
+
+namespace Disassembler {
+
+void SymbolTable::addSymbol(std::string name, uint32_t addr, uint32_t size,
+                            SymbolType type, SymbolBinding binding,
+                            std::string sectionName) {
+    table.emplace_back(name, addr, size, type, binding, sectionName);
+    nameLookup[name] = table.size() - 1;
+    addrLookup[addr].push_back(table.size() - 1);
+    sectionLookup[sectionName].push_back(table.size() - 1);
+}
+
+std::optional<std::reference_wrapper<const Symbol>> SymbolTable::getSymbolName(
+    std::string name) {
+    if (auto it = nameLookup.find(name); it != nameLookup.end()) {
+        return table[(*it).second];
+    }
+    return std::nullopt;
+}
+
+std::vector<Symbol> SymbolTable::getSymbolAddr(uint32_t addr) {
+    auto it = addrLookup.find(addr);
+    if (it == addrLookup.end()) return {};
+
+    std::vector<Symbol> ret;
+
+    for (auto i : (*it).second) {
+        ret.push_back(table[i]);
+    }
+
+    return ret;
+}
+
+std::vector<Symbol> SymbolTable::getSymbolSection(std::string sectionName) {
+    auto it = sectionLookup.find(sectionName);
+    if (it == sectionLookup.end()) return {};
+
+    std::vector<Symbol> ret;
+
+    for (auto i : (*it).second) {
+        ret.push_back(table[i]);
+    }
+
+    return ret;
+}
+}  // namespace Disassembler
