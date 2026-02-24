1c1c15ca324b37a82073e6f3237b54c0c8a15fcc
JBardoe <jackbardoe@gmail.com>
2026-02-24 14:15:15 +0000

    Added multiple entry test case

---
+++ data/test/assembly/multipleEntry.s
@@ -0,0 +1,17 @@
+.text
+.globl _start
+.globl entry_a
+.globl entry_b
+
+_start:
+    addi x1, x0, 1        
+    jal  x0, entry_a
+
+entry_a:
+    addi x2, x0, 2
+    beq  x1, x2, entry_b 
+    jal  x0, entry_b
+
+entry_b:
+    addi x3, x0, 3
+    jal  x0, entry_b
