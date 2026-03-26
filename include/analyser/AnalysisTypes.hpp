a5bb1239c34ed547a560f07bb0d2715bdbd30643
JBardoe <jackbardoe@gmail.com>
2026-03-26 18:06:24 +0000

    Added Hazard types

--- include/analyser/AnalysisTypes.hpp
+++ include/analyser/AnalysisTypes.hpp
@@ -1,6 +1,7 @@
 #ifndef ANALYSISTYPES_HPP
 #define ANALYSISTYPES_HPP
 
+#include <map>
 #include <memory>
 #include <string>
 #include <unordered_map>
@@ -11,7 +12,7 @@
 
 namespace Analyser {
 
-enum class InstructionClass {
+enum class InstructionClass : int {
     ALU,
     LOAD,
     STORE,
@@ -33,10 +34,38 @@ typedef struct InstructionAnalysis {
     int imm;
 } InstructionAnalysis;
 
+enum class HazardType : int {
+    RAW,
+    WAW,
+    WAR,
+};
+
+std::string to_string(HazardType t);
+
+enum class HazardSeverity : int {
+    HIGH,
+    MEDIUM,
+    LOW,
+};
+
+std::string to_string(HazardSeverity s);
+
+typedef struct Hazard {
+    HazardType type;
+
+    size_t first;
+    size_t second;
+
+    HazardSeverity severity;
+} Hazard;
+
+std::string to_string(Hazard h);
+
 typedef struct Analysis {
     size_t instructionCount;
     std::unique_ptr<std::unordered_map<InstructionClass, int>> instructionMix;
     std::array<int, 2> forwardBackwardBranches;
+    std::map<int, std::vector<Hazard>> hazards;
 } Analysis;
 
 std::string to_string(const Analysis& a);
